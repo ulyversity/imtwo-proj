424db433ebfba05c78047df5e0f2740b60c160e1
dawn <dawndotexe15@gmail.com>
2025-07-16 03:20:51 +0800

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
 </head>
 <body>
-    <?php
+    <div class="top-bar-div"><p>testing</p></div>
+    <div class="two-block-div">
+        <?php
         include "server/views/Dashboard_OrderStatusView.php";
         include "server/views/Dashboard_SupplyListView.php";
-    ?>
+        ?>
+    </div>
+    
 </body>
 </html>
---
+++ views/CurrentRoleView.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_SESSION['roleID']))
+{
+    require_once __DIR__."/../repository/RoleRepository.php";
+
+    $roleRepository = new RoleRepository();
+    $currentRole = $roleRepository->get($_SESSION['roleID']);
+    echo "$currentRole->Name";
+}
+else {
+    echo "<p class='role-p'>VISITOR</p>";
+}

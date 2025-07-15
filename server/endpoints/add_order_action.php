fd93fae73a072ab787e653a3092977ad9e5b8456
Ulysses O. Avellano <24105823@usc.edu.ph>
2025-07-15 22:03:23 +0800

    Implement Add Order

--- server/endpoints/add_order_action.php
+++ server/endpoints/add_order_action.php
@@ -2,20 +2,68 @@
 require_once __DIR__."/../repository/OrderRepository.php";
 require_once __DIR__."/../repository/ReceiptRepository.php";
 require_once __DIR__."/../repository/ClaimSlipRepository.php";
+require_once __DIR__."/../repository/ServiceTypeRepository.php";
 
-$ordeRepository = new OrderRepository();
-$receiptRepository = new ReceiptRepository();
-$claimSlipRepository = new ClaimSlipRepository();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ordeRepository = new OrderRepository();
+    $receiptRepository = new ReceiptRepository();
+    $claimSlipRepository = new ClaimSlipRepository();
+    $serviceTypeRepository = new ServiceTypeRepository();
+
+    $name = trim($_POST['name'] ?? '');
+    $number = trim($_POST['number'] ?? '');
+    $loadCount = trim($_POST['load'] ?? '');
+    $serviceTypeID = trim($_POST['serviceTypeID'] ?? '');
+    
+    $paid = $_POST['paid'] ?? 0;
+    $staffID = $_POST['staffID'] ?? $_SESSION['userID'];
+    $amount = $_POST['amount'] ?? 0;
+    $balance = $_POST['balance'] ?? 0;
+
+    if ($name === '' || $number === '' || $loadCount === '' || $serviceTypeID === '')
+    {
+        header("Location: ../../order.php?Error=MissingFields");
+        die();
+    }
+
+    if ($amount === 0)
+    {
+        $currentServiceType = $serviceTypeRepository->get($serviceTypeID);
+        $amount = $loadCount * $currentServiceType->ValuePerKG;
+    }
+
+    $order = new Order();
+    $order->StatusID = 1;
+    $order->ServiceTypeID = $serviceTypeID;
+    $order->StaffID = $staffID;
+    $order->LoadCount = $loadCount;
+    $order->TotalAmount = $amount;
+    $newOrderID = $ordeRepository->add($order);
+
+    $name = explode(" ", $name);
+    # REFACTOR: this will throw an error or yield unexpected result when array length != 2;
+    $firstName = $name[0];
+    $lastName = $name[1];
+
+    $claimSlip = new ClaimSlip();
+    $claimSlip->OrderID = $newOrderID;
+    $claimSlip->FirstName = $firstName;
+    $claimSlip->LastName = $lastName;
+    $claimSlip->PhoneNumber = $number;
+    $claimSlipRepository->add($claimSlip);
 
 
-$name = $_POST['name'];
-$number = $_POST['number'];
-$amount = $_POST['amount'];
-$paid = $_POST['paid'];
-$balance = $_POST['balance'];
-$loadCount = $_POST['load'];
+    if ($paid > 0)
+    {
+        if ($balance === 0)
+            $balance = max($totalAmount - $paid, 0);
 
-$order = new Order();
-$order->StaffID = $_SESSION['userID'];
-
-$ordeRepository->add();
+        $receipt = new Receipt();
+        $receipt->OrderID = $newOrderID;
+        $receipt->Due = $totalAmount;
+        $receipt->AmountPaid = $paid;
+        $receipt->Balance = $balance;
+        $receiptRepository->add($receipt);
+    }
+    header("Location: ../../order.php");
+}
--- server/endpoints/login_action.php
+++ server/endpoints/login_action.php
@@ -2,32 +2,34 @@
 declare(strict_types=1);
 require_once __DIR__."/../repository/UserRepository.php";
 
-$UserRepository = new UserRepository();
 
-$error = "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $UserRepository = new UserRepository();
+    $error = "";
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $username = trim($_POST['username'] ?? '');
+    $password = trim($_POST['password'] ?? '');
 
-if ($username === '' || $password === '')
-{
-    header("Location: ../../index.php?Error=LackingFields");
-}
+    if ($username === '' || $password === '')
+    {
+        header("Location: ../../index.php?Error=MissingFields");
+    }
 
-$userFound = $UserRepository->search("WHERE (username = '$username' OR email = '$username') AND password = '$password'");
+    $userFound = $UserRepository->search("WHERE (username = '$username' OR email = '$username') AND password = '$password'");
 
-if (count($userFound) === 0 ) {
-    $error = "IncorrectCredentials";
-}
+    if (count($userFound) === 0 ) {
+        $error = "IncorrectCredentials";
+    }
 
-if ($error == '')
-{
-    $currentUser = $userFound[0];
-    $_SESSION['userID'] = $currentUser->ID;
-    $_SESSION['roleID'] = $currentUser->RoleID;
-    $_SESSION['name'] = "$currentUser->FirstName $currentUser->LastName";
-    header("Location: ../../index.php");
-}
-else {
-    header("Location: ../../login.php?Error=".$error);
+    if ($error == '')
+    {
+        $currentUser = $userFound[0];
+        $_SESSION['userID'] = $currentUser->ID;
+        $_SESSION['roleID'] = $currentUser->RoleID;
+        $_SESSION['name'] = "$currentUser->FirstName $currentUser->LastName";
+        header("Location: ../../index.php");
+    }
+    else {
+        header("Location: ../../login.php?Error=".$error);
+    }
 }
